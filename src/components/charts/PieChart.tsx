12e18edf026922bf273b61bdbf95771847787625
sumancfc <sumanstha999@gmail.com>
2025-01-15 20:21:09 +0100

    Admin Dashboard Setup

---
+++ src/components/charts/PieChart.tsx
@@ -0,0 +1,21 @@
+'use client';
+import dynamic from 'next/dynamic';
+const Chart = dynamic(() => import('react-apexcharts'), {
+  ssr: false,
+});
+
+const PieChart = (props: any) => {
+  const { chartData, chartOptions } = props;
+
+  return (
+    <Chart
+      options={chartOptions}
+      type="pie"
+      width="100%"
+      height="100%"
+      series={chartData}
+    />
+  );
+};
+
+export default PieChart;
